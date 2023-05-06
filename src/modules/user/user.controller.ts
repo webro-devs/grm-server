f82860fd9addb7070ac93bf8b3bb4bb81b0f1e45
jaloliddin05 <nasrullayevjaloliddin.ajf@gmail.com>
2023-05-06 21:53:14 +0500

    feat: added user service and controller

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,9 +1,101 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  Controller,
+  Post,
+  Body,
+  HttpCode,
+  HttpStatus,
+  HttpException,
+  Delete,
+  Patch,
+  Param,
+  Get,
+  Query,
+} from '@nestjs/common';
+import { UpdateResult } from 'typeorm';
+import {
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiTags,
+  ApiOperation,
+} from '@nestjs/swagger';
+
+import { CreateUserDto, UpdateUserDto } from './dto';
+import { User } from './user.entity';
 import { UserService } from './user.service';
+import { PaginationDto } from '../../infra/shared/dto';
+import { Route } from '../../infra/shared/decorators/route.decorator';
 
 @ApiTags('User')
 @Controller('user')
 export class UserController {
-  constructor(private readonly adminService: UserService) {}
+  constructor(private readonly userService: UserService) {}
+
+  @Get('/')
+  @ApiOperation({ summary: 'Method: returns all users' })
+  @ApiOkResponse({
+    description: 'The users were returned successfully',
+  })
+  @HttpCode(HttpStatus.OK)
+  async getData(@Route() route: string, @Query() query: PaginationDto) {
+    try {
+      return await this.userService.getAll({ ...query, route });
+    } catch (err) {
+      throw new HttpException(err.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  @Get('/:id')
+  @ApiOperation({ summary: 'Method: returns single user by id' })
+  @ApiOkResponse({
+    description: 'The user was returned successfully',
+  })
+  @HttpCode(HttpStatus.OK)
+  async getMe(@Param('id') id: string): Promise<User> {
+    return this.userService.getOne(id);
+  }
+
+  @Post('/')
+  @ApiOperation({ summary: 'Method: creates new user' })
+  @ApiCreatedResponse({
+    description: 'The user was created successfully',
+  })
+  @HttpCode(HttpStatus.CREATED)
+  async saveData(@Body() positionData: CreateUserDto): Promise<User> {
+    try {
+      return await this.userService.create(positionData);
+    } catch (err) {
+      throw new HttpException(err.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  @Patch('/:id')
+  @ApiOperation({ summary: 'Method: updating user' })
+  @ApiOkResponse({
+    description: 'User was changed',
+  })
+  @HttpCode(HttpStatus.OK)
+  async changeData(
+    @Body() positionData: UpdateUserDto,
+    @Param('id') id: string,
+  ): Promise<UpdateResult> {
+    try {
+      return await this.userService.change(positionData, id);
+    } catch (err) {
+      throw new HttpException(err.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  @Delete('/:id')
+  @ApiOperation({ summary: 'Method: deleting user' })
+  @ApiOkResponse({
+    description: 'User was deleted',
+  })
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async deleteData(@Param('id') id: string) {
+    try {
+      return await this.userService.deleteOne(id);
+    } catch (err) {
+      throw new HttpException(err.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
 }
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,7 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { FindOptionsWhere } from 'typeorm';
+import {
+  IPaginationOptions,
+  Pagination,
+  paginate,
+} from 'nestjs-typeorm-paginate';
+
 import { User } from './user.entity';
 import { UserRepository } from './user.repository';
+import { CreateUserDto, UpdateUserDto } from './dto';
 
 Injectable();
 export class UserService {
@@ -9,4 +17,42 @@ export class UserService {
     @InjectRepository(User)
     private readonly userRepository: UserRepository,
   ) {}
+
+  async getAll(
+    options: IPaginationOptions,
+    where?: FindOptionsWhere<User>,
+  ): Promise<Pagination<User>> {
+    return paginate<User>(this.userRepository, options, {
+      order: {
+        fullName: 'ASC',
+      },
+    });
+  }
+
+  async getOne(id: string) {
+    const data = await this.userRepository.findOne({
+      where: { id },
+    });
+
+    if (!data) {
+      throw new HttpException('Data not found', HttpStatus.NOT_FOUND);
+    }
+
+    return data;
+  }
+
+  async deleteOne(id: string) {
+    const response = await this.userRepository.delete(id);
+    return response;
+  }
+
+  async change(value: UpdateUserDto, id: string) {
+    const response = await this.userRepository.update({ id }, value);
+    return response;
+  }
+
+  async create(value: CreateUserDto) {
+    const data = this.userRepository.create(value);
+    return await this.userRepository.save(data);
+  }
 }
