06a8362bd18aa57902576996e17ec62ea92cad05
siddiqun03 <siddiqun.dev@gmail.com>
2023-05-08 18:16:43 +0500

    feat: xorder dto

--- src/modules/order/dto/create-order.dto.ts
+++ src/modules/order/dto/create-order.dto.ts
@@ -1,6 +1,38 @@
-import { IsArray, IsNotEmpty, IsString, IsOptional } from 'class-validator';
-import { Transform } from 'class-transformer';
+import { IsNotEmpty, IsString, IsOptional } from 'class-validator';
+// import { Transform } from 'class-transformer';
 import { ApiProperty } from '@nestjs/swagger';
-class CreateOrderDto {}
+class CreateOrderDto {
+  @ApiProperty({
+    description: `seller`,
+    example: 'John Doe',
+  })
+  @IsNotEmpty()
+  @IsString()
+  readonly seller: string;
+
+  @ApiProperty({
+    description: `casher`,
+    example: 'Jack Richard',
+  })
+  @IsNotEmpty()
+  @IsString()
+  readonly casher: string;
+
+  @ApiProperty({
+    description: `price`,
+    example: 1500,
+  })
+  @IsNotEmpty()
+  @IsString()
+  readonly price: string;
+
+  @ApiProperty({
+    description: `date`,
+    example: '15:32GTM5z',
+  })
+  @IsNotEmpty()
+  @IsString()
+  readonly date: string;
+}
 
 export default CreateOrderDto;
--- src/modules/order/dto/update-order.dto.ts
+++ src/modules/order/dto/update-order.dto.ts
@@ -1,6 +1,38 @@
-import { IsArray, IsOptional, IsString } from 'class-validator';
-import { Transform } from 'class-transformer';
+import { IsOptional, IsString } from 'class-validator';
+// import { Transform } from 'class-transformer';
 import { ApiProperty } from '@nestjs/swagger';
-class UpdateOrderDto {}
+class UpdateOrderDto {
+  @ApiProperty({
+    description: `seller`,
+    example: 'John Doe',
+  })
+  @IsOptional()
+  @IsString()
+  readonly seller: string;
+
+  @ApiProperty({
+    description: `casher`,
+    example: 'Jack Richard',
+  })
+  @IsOptional()
+  @IsString()
+  readonly casher: string;
+
+  @ApiProperty({
+    description: `price`,
+    example: 1500,
+  })
+  @IsOptional()
+  @IsString()
+  readonly price: string;
+
+  @ApiProperty({
+    description: `date`,
+    example: '15:32GTM5z',
+  })
+  @IsOptional()
+  @IsString()
+  readonly date: string;
+}
 
 export default UpdateOrderDto;
