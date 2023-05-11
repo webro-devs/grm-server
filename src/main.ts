89468fa5425f02ade9b60927455e78ea94dc38cf
jaloliddin05 <nasrullayevjaloliddin.ajf@gmail.com>
2023-05-11 15:54:07 +0500

    feat: integrated auth guards

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
+import { AccessTokenUserGuard } from './modules/auth/passport-stratagies/access-token-user/access-token-user.guard';
+import { RolesGuard } from './modules/auth/guards/roles.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +16,26 @@ async function bootstrap() {
     .addBearerAuth()
     .addCookieAuth()
     .build();
+
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
+  app.use(cookieParser());
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      forbidUnknownValues: true,
+      transform: true,
+    }),
+  );
+  const reflector = app.get(Reflector);
+  app.useGlobalGuards(
+    new AccessTokenUserGuard(reflector),
+    new RolesGuard(reflector),
+  );
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document, {
