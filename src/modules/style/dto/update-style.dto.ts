d85d0ae6c876bfdba73549f9ed858aa59578ca22
siddiqun03 <siddiqun.dev@gmail.com>
2023-05-22 15:37:09 +0500

    fix: Style module update dto

--- src/modules/style/dto/update-style.dto.ts
+++ src/modules/style/dto/update-style.dto.ts
@@ -1,6 +1,6 @@
 import { IsString, IsOptional } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
-class UpdateStyleto {
+class UpdateStyleDto {
   @ApiProperty({
     description: `title`,
     example: 'classic',
