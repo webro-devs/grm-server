3235fbf3e8f147bf911c01d73b06d9df905ad69e
siddiqun03 <siddiqun.dev@gmail.com>
2023-05-25 06:16:49 +0500

    feat: exel validation

--- src/infra/validators/excel-data-validation.ts
+++ src/infra/validators/excel-data-validation.ts
@@ -1,21 +1,62 @@
 class ExcelDataValidation {
-  private message = {};
+  private message = {
+    error: false,
+    msg: [],
+    missingProps: [],
+  };
   constructor(private data) {}
 
   public validate() {
     this.checkProperties(this.data);
     this.checkType(this.data);
     this.checkValue(this.data);
+    this.restoreErrors();
     return this.message;
   }
 
   private checkProperties(data) {
-    //.....
+    for (const item of data) {
+      const properties = Object.keys(item);
+      const expectedProperties = [
+        'Collection',
+        'Model',
+        'Size',
+        'Color',
+        'Code',
+        'Shape',
+        'Style',
+        'Count',
+        'M2',
+      ];
+
+      if (properties.length < 9)
+        this.message.msg.push('You have less propery!');
+      else if (properties.length > 9)
+        this.message.msg.push('You have Unexpected property!');
+
+      this.message.missingProps.push(
+        ...expectedProperties.filter((item) => !properties.includes(item)),
+      );
+    }
   }
   private checkType(data) {
-    //....
+    for (const item of data) {
+      if (isNaN(item.count)) {
+        this.message.msg.push('Count should be a number.');
+      } else {
+        item.count = item.count * 1;
+      }
+    }
   }
+
   private checkValue(data) {
     //....
+  }
+
+  private restoreErrors() {
+    this.message.missingProps = [...new Set(this.message.missingProps)];
+    this.message.msg = [...new Set(this.message.msg)];
+    if (this.message.missingProps.length > 0 || this.message.msg.length > 0)
+      this.message.error = true;
   }
 }
