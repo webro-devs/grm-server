834d534a6fd9ba367c3e62df0eb3b009c2db5fe4
jaloliddin05 <nasrullayevjaloliddin.ajf@gmail.com>
2023-06-13 11:38:44 +0500

    fix: fixed partiya get by renge

--- src/infra/helpers/partiya-date-parser.ts
+++ src/infra/helpers/partiya-date-parser.ts
@@ -1,21 +1,22 @@
 const partiyaDateSort = (data) => {
-  let res = [];
-  let uniqueDate = [...new Set(data.map((d) => d.date))];
-  for (let i = 0, j = 0; i < uniqueDate.length; i++) {
-    let d = { date: uniqueDate[i], items: [] };
-    for (let k = j; k < data.length; k++) {
-      if (data[k].date == uniqueDate[i]) {
-        d.items.push(data[k]);
-        if (k == data.length - 1) {
-          res.push(d);
-        }
-      } else {
-        j = k;
-        res.push(d);
-        break;
-      }
+  data.forEach((item) => {
+    item.date = new Date(item.date);
+  });
+
+  const res = [];
+  let currentDate = null;
+  let currentGroup = null;
+
+  data.forEach((item) => {
+    const itemDate = item.date.toISOString().substr(0, 10);
+    if (itemDate !== currentDate) {
+      currentGroup = { date: itemDate, items: [] };
+      res.push(currentGroup);
+      currentDate = itemDate;
     }
-  }
+    currentGroup.items.push(item);
+  });
+
   return res;
 };
 
