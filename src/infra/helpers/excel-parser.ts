032fe9793c4f203bffbac09a5c85a75099304305
jaloliddin05 <nasrullayevjaloliddin.ajf@gmail.com>
2023-09-16 14:22:24 +0500

    fix: excel parser

--- src/infra/helpers/excel-parser.ts
+++ src/infra/helpers/excel-parser.ts
@@ -1,56 +1,54 @@
-const excelDataParser = async (data, isExcel = true) => {
-  if (isExcel) {
-    const transformedObj = data.reduce((acc, curr) => {
-      const { Collection, Model, Size, Color, Code, Count, Img, M2 } = curr;
-      const datas = {
-        Size,
-        Color,
-        Code,
-        Count,
-        Img,
-        Price: 0,
-        CommingPrice: 0,
-        Shape: '',
-        Style: '',
-        Model: '',
-        Filial: '',
-      };
+const excelDataParser = async (data) => {
+  const transformedObj = data.reduce((acc, curr) => {
+    const { Collection, Model, Size, Color, Code, Count, Img, M2 } = curr;
+    const datas = {
+      Size,
+      Color,
+      Code,
+      Count,
+      Img,
+      price: 0,
+      commingPrice: 0,
+      shape: '',
+      style: '',
+      model: '',
+      filial: '',
+    };
 
-      const collectionItem = acc.find((item) => item.title === Collection);
+    const collectionItem = acc.find((item) => item.title === Collection);
 
-      if (collectionItem) {
-        const modelItem = collectionItem.models.find(
-          (item) => item.title === Model,
-        );
+    if (collectionItem) {
+      const modelItem = collectionItem.models.find(
+        (item) => item.title === Model,
+      );
 
-        if (modelItem) {
-          modelItem.products.push(datas);
-        } else {
-          collectionItem.models.push({
-            title: Model,
-            products: [datas],
-          });
-        }
-
-        collectionItem.totalM2 = (collectionItem.totalM2 || 0) + M2;
+      if (modelItem) {
+        modelItem.products.push(datas);
       } else {
-        acc.push({
-          title: Collection,
-          models: [
-            {
-              title: Model,
-              products: [datas],
-            },
-          ],
-          totalM2: M2,
+        collectionItem.models.push({
+          title: Model,
+          products: [datas],
         });
       }
 
-      return acc;
-    }, []);
+      collectionItem.totalM2 = (collectionItem.totalM2 || 0) + M2;
+    } else {
+      acc.push({
+        title: Collection,
+        models: [
+          {
+            title: Model,
+            products: [datas],
+          },
+        ],
+        totalM2: M2,
+      });
+    }
 
-    return transformedObj;
-  }
+    return acc;
+  }, []);
+
+  return transformedObj;
 };
 
 export default excelDataParser;
