ffb3c066b55bd1cd993df412fc595529f89b65cd
siddiqun03 <siddiqun.dev@gmail.com>
2024-02-10 11:49:55 +0500

    fix

--- src/modules/cashflow/cashflow.service.ts
+++ src/modules/cashflow/cashflow.service.ts
@@ -181,7 +181,7 @@ export class CashflowService {
 
       if (value.type == 'Расход') {
         if (kassa.totalSum < value.price) throw new BadRequestException('You dont have enough money!');
-        if (value.title == 'Магазин Приход') {
+        if (value.title == 'Магазин Расход') {
           kassa.expenditureBoss = kassa.expenditureBoss + value.price;
         } else {
           kassa.expenditureShop = kassa.expenditureShop + value.price;
