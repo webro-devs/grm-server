d2a09452c8da6a63ccdca05ed5be1e91355a4526
siddiqun03 <siddiqun.dev@gmail.com>
2024-02-15 21:41:09 +0500

    fix

--- src/modules/order/order.service.ts
+++ src/modules/order/order.service.ts
@@ -313,7 +313,13 @@ export class OrderService {
     if (order.isActive === OrderEnum.Reject) throw new BadRequestException('Already Rejected!');
     await this.returnProduct(order.product, order.x, order.x);
 
-    await this.addCashFlow(order.price, order.kassa.id, CashflowExpenditureEnum.BOSS, CashFlowEnum.Consumption, userId);
+    await this.addCashFlow(
+      order.price + order.plasticSum,
+      order.kassa.id,
+      CashflowExpenditureEnum.BOSS,
+      CashFlowEnum.Consumption,
+      userId,
+    );
 
     await this.orderRepository.update({ id: order.id }, { isActive: OrderEnum.Reject });
 
