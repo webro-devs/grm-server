14d648614aa957c8436bd46d35d15bd666ecf659
siddiqun03 <siddiqun.dev@gmail.com>
2024-03-14 14:50:51 +0500

    fix

--- src/modules/partiya/dto/update-partiya.dto.ts
+++ src/modules/partiya/dto/update-partiya.dto.ts
@@ -1,4 +1,4 @@
-import { IsOptional, IsString, IsNumber } from 'class-validator';
+import { IsOptional, IsString, IsNumber, IsBoolean } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 class UpdatePartiyaDto {
   @ApiProperty({
@@ -34,7 +34,7 @@ class UpdatePartiyaDto {
     required: false,
   })
   @IsOptional()
-  @IsNumber()
+  @IsBoolean()
   readonly check?: boolean;
 }
 
