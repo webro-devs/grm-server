241ddb882e4db4586a38a587eb750567db5ec5ff
siddiqun03 <siddiqun.dev@gmail.com>
2024-04-12 11:17:00 +0500

    fix

--- src/infra/middleware/transfer-query-parser.ts
+++ src/infra/middleware/transfer-query-parser.ts
@@ -40,7 +40,7 @@ class TransferQueryParserMiddleware implements NestMiddleware {
 
     if (progress) {
       try {
-        const transformedData = JSON.parse(progress);
+        const transformedData = progress.split('_');
         where.progres = In(transformedData);
       } catch (e) {
         throw new BadRequestException(e.message);
