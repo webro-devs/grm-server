09335d6d9d30ded1b797cb31df6fcca5bbbe7080
siddiqun03 <siddiqun.dev@gmail.com>
2024-04-16 17:03:11 +0500

    fix

--- src/modules/product/utils/product-supports.util.ts
+++ src/modules/product/utils/product-supports.util.ts
@@ -1,38 +1,38 @@
 const query = () => `
 with color as (select distinct title, count(title), jsonb_agg(distinct color.id) as id
                from color
-                        join product as p on color.id = p."colorId"
+                        left join product as p on color.id = p."colorId"
                where "isInternetShop" = true
                group by title),
      style as (select distinct title, count(title), jsonb_agg(distinct style.id) as id
                from style
-                        join product as p on style.title = p.style
+                        left join product as p on style.title = p.style
                where "isInternetShop" = true
                group by title),
      shape as (select distinct title, count(title), jsonb_agg(distinct shape.id) as id
                from shape
-                        join product as p on shape.title = p.shape
+                        left join product as p on shape.title = p.shape
                where "isInternetShop" = true
                group by title),
      size as (select distinct title, count(title), jsonb_agg(distinct size.id) as id
               from size
-                       join product as p on size.title = p.size
+                       left join product as p on size.title = p.size
               where "isInternetShop" = true
               group by title),
      country as (select distinct title, count(title), jsonb_agg(distinct country.id) as id
                  from country
-                          join product as p on country.title = p.country
+                          left join product as p on country.title = p.country
                  where "isInternetShop" = true
                  group by title),
      collection as (select distinct collection.title, count(collection.title), jsonb_agg(distinct collection.id) as id, jsonb_agg(distinct m.title) as model
                     from collection
-                             join model as m on collection.id = m."collectionId"
-                             join product as p on m.id = p."modelId"
+                             left join model as m on collection.id = m."collectionId"
+                             left join product as p on m.id = p."modelId"
                     where "isInternetShop" = true
                     group by collection.title),
      model as (select distinct title, count(title), jsonb_agg(distinct model.id) as id
                from model
-                        join product as p on model.id = p."modelId"
+                        left join product as p on model.id = p."modelId"
                where "isInternetShop" = true
                group by title)
 select json_agg(distinct model)      as model,
