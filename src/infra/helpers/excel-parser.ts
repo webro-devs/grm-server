cf5748aa009baf34e7d3349b996ba3e860879353
siddiqun03 <siddiqun.dev@gmail.com>
2024-04-28 17:26:07 +0500

    fix

--- src/infra/helpers/excel-parser.ts
+++ src/infra/helpers/excel-parser.ts
@@ -1,3 +1,102 @@
+// const excelDataParser = (data, expense) => {
+//   let allM2 = 0;
+//   const transformedObj = data.reduce((acc, curr) => {
+//     const {
+//       id,
+//       code,
+//       size,
+//       plate,
+//       color,
+//       count,
+//       shape,
+//       style,
+//       model,
+//       imgUrl,
+//       otherImgs = [],
+//       collection,
+//       filial = '',
+//       commingPrice = 0,
+//       priceMeter = 0,
+//       secondPrice = 0,
+//       country = 'пустой',
+//       isEdite = false,
+//     } = curr;
+//     const m2 =
+//       (eval(size.title.match(/\d+\.*\d*/g).join('*')) / 10000) * count || 0;
+//     allM2 += m2;
+//
+//     const datas = {
+//       isEdite,
+//       id,
+//       size,
+//       color,
+//       code,
+//       count,
+//       imgUrl,
+//       commingPrice,
+//       shape,
+//       style,
+//       filial,
+//       plate,
+//       m2,
+//       otherImgs,
+//       priceMeter,
+//       secondPrice,
+//       price: (priceMeter + secondPrice) * m2,
+//       country,
+//     };
+//
+//     const collectionItem = acc.find((item) => item.title === collection.title);
+//
+//     if (collectionItem) {
+//       const modelItem = collectionItem.models.find(
+//         (item) => item.title === model.title,
+//       );
+//
+//       if (modelItem) {
+//         modelItem.m2 += m2;
+//         modelItem.products.push(datas);
+//         collectionItem.collection_m += m2;
+//       } else {
+//         collectionItem.models.push({
+//           id: model.id,
+//           title: model.title,
+//           cost: priceMeter,
+//           commingPrice: commingPrice,
+//           m2: m2,
+//           products: [datas],
+//         });
+//       }
+//       collectionItem.collection_m += m2;
+//     } else {
+//       acc.push({
+//         id: collection.id,
+//         title: collection.title,
+//         collection_cost: 0,
+//         collection_exp: 0,
+//         collection_m: m2,
+//         models: [
+//           {
+//             id: model.id,
+//             title: model.title,
+//             cost: priceMeter,
+//             commingPrice: commingPrice,
+//             m2: m2,
+//             products: [datas],
+//           },
+//         ],
+//       });
+//     }
+//
+//     return acc;
+//   }, []);
+//   transformedObj.forEach((el, index) => {});
+//
+//   return transformedObj;
+// };
+//
+// export default excelDataParser;
+
 const excelDataParser = (data, expense) => {
   let allM2 = 0;
   const transformedObj = data.reduce((acc, curr) => {
@@ -21,8 +120,10 @@ const excelDataParser = (data, expense) => {
       country = 'пустой',
       isEdite = false,
     } = curr;
-    const m2 =
-      (eval(size.title.match(/\d+\.*\d*/g).join('*')) / 10000) * count || 0;
+
+    // Avoid using eval()
+    const sizeMatches = size.title.match(/\d+\.*\d*/g);
+    const m2 = sizeMatches ? (sizeMatches.reduce((acc, val) => acc * parseFloat(val), 1) / 10000) * count : 0;
     allM2 += m2;
 
     const datas = {
@@ -46,11 +147,11 @@ const excelDataParser = (data, expense) => {
       country,
     };
 
-    const collectionItem = acc.find((item) => item.title === collection.title);
+    const collectionItem = acc.find((item) => item.title === (collection ? collection.title : null));
 
     if (collectionItem) {
       const modelItem = collectionItem.models.find(
-        (item) => item.title === model.title,
+        (item) => item.title === (model ? model.title : null),
       );
 
       if (modelItem) {
@@ -59,8 +160,8 @@ const excelDataParser = (data, expense) => {
         collectionItem.collection_m += m2;
       } else {
         collectionItem.models.push({
-          id: model.id,
-          title: model.title,
+          id: model ? model.id : null,
+          title: model ? model.title : null,
           cost: priceMeter,
           commingPrice: commingPrice,
           m2: m2,
@@ -70,15 +171,15 @@ const excelDataParser = (data, expense) => {
       collectionItem.collection_m += m2;
     } else {
       acc.push({
-        id: collection.id,
-        title: collection.title,
+        id: collection ? collection.id : null,
+        title: collection ? collection.title : null,
         collection_cost: 0,
         collection_exp: 0,
         collection_m: m2,
         models: [
           {
-            id: model.id,
-            title: model.title,
+            id: model ? model.id : null,
+            title: model ? model.title : null,
             cost: priceMeter,
             commingPrice: commingPrice,
             m2: m2,
@@ -90,9 +191,9 @@ const excelDataParser = (data, expense) => {
 
     return acc;
   }, []);
-  transformedObj.forEach((el, index) => {});
 
-  return transformedObj;
+  // Initialize transformedObj as an object instead of an array
+  return Object.values(transformedObj);
 };
 
 export default excelDataParser;
