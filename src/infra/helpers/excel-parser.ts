2c0ff6bb20c4748d044c28676fb3c124af249fe2
siddiqun03 <siddiqun.dev@gmail.com>
2024-04-28 18:00:47 +0500

    fix

--- src/infra/helpers/excel-parser.ts
+++ src/infra/helpers/excel-parser.ts
@@ -1,98 +1,97 @@
 const excelDataParser = (data, expense) => {
-  let allM2 = 0;
-  const transformedObj = data.reduce((acc, curr) => {
-    const {
-      id,
-      code,
-      size,
-      plate,
-      color,
-      count,
-      shape,
-      style,
-      model,
-      imgUrl,
-      otherImgs = [],
-      collection,
-      filial = '',
-      commingPrice = 0,
-      priceMeter = 0,
-      secondPrice = 0,
-      country = 'пустой',
-      isEdite = false,
-    } = curr;
-    const m2 =
-      (eval(size.title.match(/\d+\.*\d*/g).join('*')) / 10000) * count || 0;
-    allM2 += m2;
+let allM2 = 0;
+const transformedObj = data.reduce((acc, curr) => {
+  const {
+    id,
+    code,
+    size,
+    plate,
+    color,
+    count,
+    shape,
+    style,
+    model,
+    imgUrl,
+    otherImgs = [],
+    collection,
+    filial = '',
+    commingPrice = 0,
+    priceMeter = 0,
+    secondPrice = 0,
+    country = 'пустой',
+    isEdite = false,
+  } = curr;
+  const m2 =
+    (eval(size.title.match(/\d+\.*\d*/g).join('*')) / 10000) * count || 0;
+  allM2 += m2;
 
-    const datas = {
-      isEdite,
-      id,
-      size,
-      color,
-      code,
-      count,
-      imgUrl,
-      commingPrice,
-      shape,
-      style,
-      filial,
-      plate,
-      m2,
-      otherImgs,
-      priceMeter,
-      secondPrice,
-      price: (priceMeter + secondPrice) * m2,
-      country,
-    };
+  const datas = {
+    isEdite,
+    id,
+    size,
+    color,
+    code,
+    count,
+    imgUrl,
+    commingPrice,
+    shape,
+    style,
+    filial,
+    plate,
+    m2,
+    otherImgs,
+    priceMeter,
+    secondPrice,
+    price: (priceMeter + secondPrice) * m2,
+    country,
+  };
 
-    const collectionItem = acc.find((item) => item.title === collection.title);
+  const collectionItem = acc.find((item) => item.title === collection.title);
 
-    if (collectionItem) {
-      const modelItem = collectionItem.models.find(
-        (item) => item.title === model.title,
-      );
+  if (collectionItem) {
+    const modelItem = collectionItem.models.find(
+      (item) => item.title === model.title,
+    );
 
-      if (modelItem) {
-        modelItem.m2 += m2;
-        modelItem.products.push(datas);
-        collectionItem.collection_m += m2;
-      } else {
-        collectionItem.models.push({
+    if (modelItem) {
+      modelItem.m2 += m2;
+      modelItem.products.push(datas);
+      collectionItem.collection_m += m2;
+    } else {
+      collectionItem.models.push({
+        id: model.id,
+        title: model.title,
+        cost: priceMeter,
+        commingPrice: commingPrice,
+        m2: m2,
+        products: [datas],
+      });
+    }
+    collectionItem.collection_m += m2;
+  } else {
+    acc.push({
+      id: collection.id,
+      title: collection.title,
+      collection_cost: 0,
+      collection_exp: 0,
+      collection_m: m2,
+      models: [
+        {
           id: model.id,
           title: model.title,
           cost: priceMeter,
           commingPrice: commingPrice,
           m2: m2,
           products: [datas],
-        });
-      }
-      collectionItem.collection_m += m2;
-    } else {
-      acc.push({
-        id: collection.id,
-        title: collection.title,
-        collection_cost: 0,
-        collection_exp: 0,
-        collection_m: m2,
-        models: [
-          {
-            id: model.id,
-            title: model.title,
-            cost: priceMeter,
-            commingPrice: commingPrice,
-            m2: m2,
-            products: [datas],
-          },
-        ],
-      });
-    }
+        },
+      ],
+    });
+  }
 
-    return acc;
-  }, []);
-  transformedObj.forEach((el, index) => {});
+  return acc;
+}, []);
 
-  return transformedObj;
+return transformedObj;
 };
 
 export default excelDataParser;
