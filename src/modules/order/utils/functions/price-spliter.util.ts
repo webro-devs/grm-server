203799449241de153411f06641a1681671db6352
siddiqun03 <siddiqun.dev@gmail.com>
2024-05-20 20:28:07 +0500

    fix

--- src/modules/order/utils/functions/price-spliter.util.ts
+++ src/modules/order/utils/functions/price-spliter.util.ts
@@ -1,6 +1,6 @@
 function getParts(num) {
   let integerPart = Math.trunc(num);
-  let decimalPart = Number(String(num).split('.')[1]) || 0;
+  let decimalPart = Number( "0." + String(num).split('.')[1]) || 0;
 
   return {
     integerPart: integerPart,
