046f781170930256c86a275c2c56d26cc9604d87
siddiqun03 <siddiqun.dev@gmail.com>
2024-06-06 20:52:21 +0500

    fixed bug

--- src/modules/product/utils/internet-shop.util.ts
+++ src/modules/product/utils/internet-shop.util.ts
@@ -4,7 +4,7 @@ from product as p
          left join model as m on p."modelId" = m.id
          left join collection as c on m."collectionId" = c.id
 where "isInternetShop" = true
-group by c.title, m.title;
+group by c.title;
 `
 
 export default query
