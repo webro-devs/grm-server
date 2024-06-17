f35043a588e6210b81c8b9c635234cbd7d27781a
siddiqun03 <siddiqun.dev@gmail.com>
2024-06-17 09:48:10 +0500

    fix

--- src/infra/middleware/kassa-query-parser.ts
+++ src/infra/middleware/kassa-query-parser.ts
@@ -6,9 +6,13 @@ import { Between, LessThanOrEqual, MoreThanOrEqual } from 'typeorm';
 class KassaQueryParserMiddleware implements NestMiddleware {
   use(req, res: Response, next: NextFunction) {
     let where: any = {};
-    const { startDate, endDate, filial, total } = req.query;
+    let { startDate, endDate, filial, total } = req.query;
 
     if (startDate && endDate) {
+      startDate = new Date(startDate);
+      endDate = new Date(startDate);
+      startDate.setHours(0, 0, 0, 0);
+      endDate.setHours(23, 59, 59, 999);
       where = {
         startDate: Between(
           (new Date(startDate)).setHours(0, 0, 0, 0),
@@ -16,10 +20,15 @@ class KassaQueryParserMiddleware implements NestMiddleware {
         ),
       };
     } else if (startDate) {
+      startDate = new Date(startDate);
+      startDate.setHours(0, 0, 0, 0);
+
       where = {
         startDate: MoreThanOrEqual((new Date(startDate)).setHours(0, 0, 0, 0)),
       };
     } else if (endDate) {
+      endDate = new Date(startDate);
+      endDate.setHours(23, 59, 59, 999);
       where = {
         startDate: LessThanOrEqual((new Date(endDate)).setHours(23, 59, 59, 999)),
       };
