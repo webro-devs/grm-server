79a17c9e75c60da20e3401f89ce2dc559425742e
siddiqun03 <siddiqun.dev@gmail.com>
2024-06-26 17:22:42 +0500

    fix

--- src/modules/product/utils/internet-shop.util.ts
+++ src/modules/product/utils/internet-shop.util.ts
@@ -3,7 +3,7 @@ select distinct array_agg("imgUrl") as images, c.title, min("secondPrice") as pr
 from product as p
          left join model as m on p."modelId" = m.id
          left join collection as c on m."collectionId" = c.id
-where "isInternetShop" = true
+where "isInternetShop" = true and count > 0
 group by c.title;
 `
 
